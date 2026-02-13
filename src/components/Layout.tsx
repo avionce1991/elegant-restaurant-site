8d65ab1c7cdd017ff8cdf6e96425a6857cd1e186
unknown <aleksandar.nicic@badin.rs>
2026-02-13 01:17:01 +0100

    resen problem za horizontal gallery

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="min-h-screen flex flex-col overflow-x-hidden">
+    <div className="min-h-screen flex flex-col">
       <Header />
       <div className="flex-1">{children}</div>
       <Footer />
