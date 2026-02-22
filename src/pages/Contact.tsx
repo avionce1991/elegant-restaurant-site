865f4883ff303a7409c7b567dc18cc235e0826a7
unknown <aleksandar.nicic@badin.rs>
2026-02-22 22:53:17 +0100

    bring back contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -21,7 +21,7 @@ const Contact = () => {
     };
 
     try {
-      const res = await fetch("https://an-photography-website.onrender.com", {
+      const res = await fetch("https://an-photography-website.onrender.com/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
